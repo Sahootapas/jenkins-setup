fbf61c92978ab7b131f870858d09d8fdf4a2fecb
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-04-22 17:41:10 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#This script is used in RHEL, CentOS, AWS Linux to deploy Jenkins 
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
